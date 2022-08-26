36c67c54a89b83140b302f5d3caf7e6d0e1fac55
Jon <90407167+HaolinZhong@users.noreply.github.com>
2022-08-26 19:44:41 -0400

    Time: 28 ms (97.88%), Space: 13.9 MB (79.51%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        
+        M =  min(k * (k + 1) // 2, 9)
+        
+        if n < M: return []
+        
+        cur = []
+        ans = []
+        
+        def dfs(D, curSum):
+            
+            if curSum == n and len(cur) == k:
+                ans.append(list(cur))
+                return
+            
+            if D > 9: return
+            
+            cur.append(D)
+            dfs(D + 1, curSum + D)
+            cur.pop()
+            dfs(D + 1, curSum)
+        
+        dfs(1, 0)
+        
+        return ans
