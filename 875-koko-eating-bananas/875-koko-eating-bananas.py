4ec09049b587395773445529660ecdd90a6c2550
Jon <90407167+HaolinZhong@users.noreply.github.com>
2022-09-04 17:23:16 -0400

    Time: 1114 ms (9.00%), Space: 15.4 MB (97.24%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        def canEat(k):
+            T = 0
+            for n in piles:
+                T += n // k
+                if n % k != 0: T += 1
+            return T <= h
+        
+        l, r = 1, int(1e9)
+        
+        while l < r:
+            m = (l + r) // 2
+            if canEat(m):
+                r = m
+            else:
+                l = m + 1
+        
+        return l
+        
