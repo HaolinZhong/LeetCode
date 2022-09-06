d70250c4ee11b4c11484ec964e3fed86050cb34b
Jon <90407167+HaolinZhong@users.noreply.github.com>
2022-09-05 22:31:05 -0400

    Time: 411 ms (7.01%), Space: 15 MB (10.19%) - LeetHub

---
+++ 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.py
@@ -0,0 +1,23 @@
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        
+        nums.sort()
+        
+        def getSmallerPairN(x):
+            j = 0
+            cnt = 0
+            for i in range(len(nums)):
+                while j < len(nums) and nums[j] - nums[i] <= x: j += 1
+                cnt += j - i - 1
+            return cnt
+        
+        l, r = 0, nums[-1] - nums[0]
+        
+        while l < r:
+            m = (l + r) // 2
+            if getSmallerPairN(m) < k:
+                l = m + 1
+            else:
+                r = m
+        
+        return l
