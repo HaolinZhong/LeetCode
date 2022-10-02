f6d7ec3ffaa07bb5282b50c0115f01a90d4e9897
Jon <90407167+HaolinZhong@users.noreply.github.com>
2022-10-02 15:15:16 -0400

    Time: 8752 ms (12.22%), Space: 24.3 MB (69.21%) - LeetHub

--- 827-making-a-large-island/827-making-a-large-island.py
+++ 827-making-a-large-island/827-making-a-large-island.py
@@ -1,8 +1,10 @@
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
         
+        # 四个方向
         DIR = [0, 1, 0, -1, 0]
         
+        # 给island赋予指定的id, 并返回island面积
         def paint(i, j, color):
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != 1: 
                 return 0
@@ -12,6 +14,8 @@ class Solution:
                 ans += paint(i+DIR[k], j+DIR[k+1], color)
             return ans
         
+        
+        # 给island赋予独特的id, 记录每个island面积, 并求: 不执行转换时的最大island
         color = 2
         maxArea = 0
         islandArea = {}
@@ -22,19 +26,17 @@ class Solution:
                 maxArea = max(maxArea, curArea)
                 color += 1
         
-        def getSize(i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
-                return 0
-            return islandArea[grid[i][j]]
-        
+        # 对于为0的区域, 查看其周围四个方向的island编号
         for i, j in product(range(len(grid)), range(len(grid[0]))):
             if grid[i][j] == 0:
+                # 使用set避免重复计算同一个island, 将周围island id记录下来
                 nbs = set()
                 for k in range(4):
                     if i + DIR[k] < 0 or i + DIR[k] >= len(grid) or j + DIR[k+1] < 0 or j + DIR[k+1] >= len(grid[0]) or grid[i+DIR[k]][j+DIR[k+1]] == 0:
                         continue
                     nbs.add(grid[i+DIR[k]][j+DIR[k+1]])
                 
+                # 根据id来求得面积
                 curArea = 1
                 for n in nbs:
                     curArea += islandArea[n]
