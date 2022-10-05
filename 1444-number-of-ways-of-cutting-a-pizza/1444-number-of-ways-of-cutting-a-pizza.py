89f4762f5a50df295955bd051ef16d47f5fd2b93
Jon <90407167+HaolinZhong@users.noreply.github.com>
2022-10-04 22:54:37 -0400

    Time: 755 ms (25.00%), Space: 16.1 MB (35.94%) - LeetHub

---
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.py
@@ -0,0 +1,25 @@
+class Solution:
+    def ways(self, pizza: List[str], k: int) -> int:
+        m, n, MOD = len(pizza), len(pizza[0]), 10 ** 9 + 7
+        presum = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                presum[i][j] = presum[i+1][j] + presum[i][j+1] - presum[i+1][j+1] + (pizza[i][j] == 'A')
+        
+        @lru_cache(None)
+        def dp(i, j, k):
+            if presum[i][j] == 0: return 0
+            if k == 0: return 1
+            ans = 0
+            for ni in range(i+1, m):
+                if presum[i][j] - presum[ni][j] > 0:
+                    ans = (ans + dp(ni, j, k-1)) % MOD
+            
+            for nj in range(j+1, n):
+                if presum[i][j] - presum[i][nj] > 0:
+                    ans = (ans + dp(i, nj, k-1)) % MOD
+            
+            return ans
+        
+        return dp(0, 0, k - 1)
