eee9e939b305f97cca5cb21112fd111fcc83857e
Jon <90407167+HaolinZhong@users.noreply.github.com>
2022-10-04 20:21:30 -0400

    Time: 3639 ms (25.35%), Space: 14.7 MB (33.47%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,20 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [-1] * (amount+1)
+        dp[0] = 0
+        
+        coins.sort()
+        
+        for i in range(1, amount+1):
+            cur_min = sys.maxsize
+            for n in coins:
+                if i < n: break
+                if dp[i-n] == -1: continue
+                cur_min = min(cur_min, dp[i-n] + 1)
+            if cur_min == sys.maxsize:
+                dp[i] = -1
+            else:
+                dp[i] = cur_min
+        print(dp)
+        return dp[-1]
+        
