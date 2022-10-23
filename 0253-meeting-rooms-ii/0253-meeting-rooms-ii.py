fcda312688b6e2caa1ac1897a52b7195dac271ab
Jon <90407167+HaolinZhong@users.noreply.github.com>
2022-10-23 01:21:36 -0400

    Time: 295 ms (5.03%), Space: 18.2 MB (5.67%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -2,19 +2,21 @@ from sortedcontainers import SortedList
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals.sort()
-        rooms = [[intervals[0][1]]]
-        find = True
+        rooms = SortedList()
+        rooms.add(intervals[0][1])
+        
         for i in range(1, len(intervals)):
-            find = False
-            for j in range(len(rooms)):
-                if intervals[i][0] < rooms[j][-1]:
-                    continue
-                rooms[j].append(intervals[i][1])
-                find = True
-                break
-            if not find: rooms.append([intervals[i][1]])
+            start, end = intervals[i]
+            ind = rooms.bisect_right(start)
+            if ind == 0: 
+                rooms.add(end)
+            else:
+                rooms.pop(0)
+                rooms.add(end)
+            print(rooms)
         
         return len(rooms)
+            
             
         
         
