1e2b3647995fe732a7dada218c208c92f389456b
Jon <90407167+HaolinZhong@users.noreply.github.com>
2022-11-08 16:35:21 -0500

    Time: 178 ms (30.75%), Space: 14.1 MB (50.40%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,23 +3,19 @@ class Solution:
         
         if len(s) == 0: return 0
         
-        d = defaultdict(int)
+        d = {}
         max_len = 1
         l, r = 0, 0
         
         while r < len(s):
-            while r < len(s) and d[s[r]] == 0:
-                d[s[r]] += 1
+            while r < len(s) and (s[r] not in d.keys() or d[s[r]] < l):
+                d[s[r]] = r
                 r += 1
             
             max_len = max(max_len, r - l)
             if r == len(s): break
-            d[s[r]] += 1
-            
-            while d[s[r]] > 1 and l <= r:
-                d[s[l]] -= 1
-                l += 1
-            
+            l = d[s[r]] + 1
+            d[s[r]] = r
             r += 1
         
         return max_len
