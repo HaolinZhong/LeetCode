d26ca7367c1db05e62143219612590d36938f2e2
Jon <90407167+HaolinZhong@users.noreply.github.com>
2022-11-08 19:31:47 -0500

    Time: 313 ms (42.81%), Space: 14.7 MB (36.75%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        window, need = defaultdict(int), defaultdict(int)
+        l, r, valid = 0, 0, 0
+        
+        for c in t: need[c] += 1
+        
+        minstr = ""
+        minlen = len(s) + 1
+        
+        while r < len(s):
+            c = s[r]
+            r += 1
+            
+            if c in need.keys():
+                window[c] += 1
+                if window[c] == need[c]:
+                    valid += 1
+            
+            while valid == len(need):
+                if (r - l) < minlen:
+                    minlen = r - l
+                    minstr = s[l:r]
+                
+                d = s[l]
+                l += 1
+                if d in need.keys():
+                    window[d] -= 1
+                    if window[d] == need[d] - 1:
+                        valid -= 1
+            
+        return minstr
+            
+            
