41ca1b2d11d289099e75289a2c0ab066349c76a1
Jon <90407167+HaolinZhong@users.noreply.github.com>
2023-01-31 17:20:33 -0500

    Time: 1 ms (100.00%), Space: 52 MB (40.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,22 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int[] pfSum = new int[nums.length + 1];
-        pfSum[0] = 0;
-        int[] leftMin = new int[nums.length + 1];
-        leftMin[0] = 0;
-        int lmin = 0;
-        for (int i = 1; i < nums.length + 1; i++) {
-            pfSum[i] = nums[i-1] + pfSum[i-1];
-            if (pfSum[i] < lmin) lmin = pfSum[i];
-            leftMin[i] = lmin;
-        }
+        int curSum = nums[0];
+        int maxSum = nums[0];
         
-        int maxSum = Integer.MIN_VALUE;
-        for (int i = 1; i < nums.length + 1; i++) {
-            maxSum = pfSum[i] - leftMin[i-1] > maxSum ? pfSum[i] - leftMin[i-1] : maxSum;
+        // curSum: 必须包括i, 以其为终点的所有subarray中最优的subarray
+        for (int i = 1; i < nums.length; i++) {
+            curSum = curSum < 0 ? nums[i] : curSum + nums[i];
+            maxSum = maxSum < curSum ? curSum : maxSum;
         }
         
         return maxSum;
-        
     }
 }
