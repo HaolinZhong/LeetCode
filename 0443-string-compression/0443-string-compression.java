e157f85a2b80ce28ee91f77e07deccb9b027251f
Jon <90407167+HaolinZhong@users.noreply.github.com>
2023-08-21 13:19:07 -0400

    Time: 2 ms (29.76%), Space: 42.9 MB (85.46%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,6 +1,5 @@
 class Solution {
     public int compress(char[] chars) {
-        StringBuilder sb = new StringBuilder();
         char prev = '#';
         int cnt = 0;
         int i = 0; int j = 0;
@@ -13,15 +12,8 @@ class Solution {
             }
             chars[i++] = c;
             if (cnt == 1) continue;
-            int[] cLen = new int[]{-1, -1, -1, -1};
-            int k = 3;
-            while (cnt > 0) {
-                cLen[k--] = cnt % 10;
-                cnt /= 10;
-            }
-            for (int x = 0; x < 4; x++) {
-                if (cLen[x] == -1) continue;
-                chars[i++] = (char) (cLen[x] + '0');
+            for (char a : Integer.toString(cnt).toCharArray()) {
+                chars[i++] = a;
             }
         }
         return i;
