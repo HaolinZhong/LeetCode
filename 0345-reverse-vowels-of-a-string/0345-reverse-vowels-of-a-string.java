f62e468d4a80ec48b9384a577362a218e1344aaa
Jon <90407167+HaolinZhong@users.noreply.github.com>
2023-08-21 11:00:25 -0400

    Time: 56 ms (17.17%), Space: 44.2 MB (36.38%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -21,6 +21,7 @@ class Solution {
     
     private boolean isVowel(char c) {
         c = Character.toLowerCase(c);
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'; 
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' 
+            || c == 'O' || c == 'U'; 
     }
 }
