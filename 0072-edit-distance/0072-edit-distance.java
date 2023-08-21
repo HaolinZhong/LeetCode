eb435602c33bf13f52e7f9d2166ae17b24103fcb
Jon <90407167+HaolinZhong@users.noreply.github.com>
2023-08-21 13:34:36 -0400

    Time: 5 ms (70.38%), Space: 43.3 MB (95.35%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,33 +1,27 @@
 class Solution {
-    
-    private Integer[][] mem;
-    
     public int minDistance(String word1, String word2) {
-        int m = word1.length(); int n = word2.length();
-        mem = new Integer[m][n];
-        return dp(word1, word2, 0, 0);
-    }
-    
-    private int dp(String s1, String s2, int i, int j) {
-        int m = s1.length(); int n = s2.length();
-        if (i >= m) return n - j;
-        if (j >= n) return m - i;
-
-        if (mem[i][j] != null) return mem[i][j];
-        
-        if (s1.charAt(i) == s2.charAt(j)) {
-            mem[i][j] = dp(s1, s2, i+1, j+1);
-            return mem[i][j];
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        // word1 i   word2 j  equal  num = dp[i - 1][j - 1]
+        //init
+        for (int i = 1; i <= word1.length(); i++) {
+            //j == 0
+            dp[i][0] = i;
         }
+        for (int j = 0; j <= word2.length(); j++) {
+            dp[0][j] = j;
+        }
+        // word1 i-1 == word2 j-1     dp[i][j] = Math.min(dp[i - 1][j]+1, dp[i][j-1]+1, dp[i-1][j-1])     
+        //word1 i-1 == word2 j-1     dp[i][j] = Math.min(dp[i - 1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+1)
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                int temp = 0;
+                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+                    temp = 1;
+                }
+                dp[i][j] = Math.min(dp[i - 1][j] + 1, Math.min(dp[i][j - 1] + 1, dp[i - 1][j - 1] + temp));   
+            }
+        }
+        return dp[word1.length()][word2.length()];
         
-        // insert at s1
-        int op1 = dp(s1, s2, i, j+1) + 1;
-        // delete at s1
-        int op2 = dp(s1, s2, i+1, j) + 1;
-        // replace at s1
-        int op3 = dp(s1, s2, i+1, j+1) + 1;
-        
-        mem[i][j] = Math.min(Math.min(op1, op2), op3);
-        return mem[i][j];
     }
 }
