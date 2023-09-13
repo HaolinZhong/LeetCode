8f6926119944df627c5121597ab3c752bf85c669
Jon <90407167+HaolinZhong@users.noreply.github.com>
2023-09-13 13:46:47 -0400

    Time: 2 ms (9.45%), Space: 39.3 MB (47.52%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -1,10 +1,26 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) k--;
-            if (k == 0) return i;
+        List<Integer> divisors = new ArrayList();
+        int sqrtN = (int) Math.sqrt(n);
+        for (int x = 1; x < sqrtN + 1; ++x) {
+            if (n % x == 0) {
+                --k;
+                divisors.add(x);
+                if (k == 0) {
+                    return x;    
+                }    
+            }    
         }
         
-        return -1;
+        // If n is a perfect square
+        // we have to skip the duplicate 
+        // in the divisor list
+        if (sqrtN * sqrtN == n) {
+            ++k;    
+        }
+                
+        int nDiv = divisors.size();
+        System.out.println(nDiv);
+        return (k <= nDiv) ? n / divisors.get(nDiv - k) : -1;
     }
 }
